eb8e1f9b1cb6750aa662591f75e6bfd30355f7c6
Kristopher Ives <kristopher.ives@gmail.com>
2018-08-29 13:32:14 +0000

    Added edit.html template. Added edit.php page.

---
+++ pages/edit.php
@@ -0,0 +1,45 @@
+<?php
+
+use \RedBeanPHP\R as R;
+
+$account = $this->getAccount();
+$post = $this->getPost(@intval($_GET['id']));
+
+if (empty($post)) {
+  $this->redirect("dashboard");
+}
+
+if ($post->account->id !== $account->id) {
+  trigger_error("Attempt to edit someone elses post!");
+  $this->redirect("dashboard");
+}
+
+if (isset($_POST['delete'])) {
+  R::trash($post);
+  $this->redirect("dashboard");
+}
+
+if (@strlen($post->url) > 0) {
+  $this->redirect("dashboard");
+}
+
+if (!isset($_POST['submit'])) {
+  $post->whendate = $this->formatUserDate(intval($post->when), $post->whenzone ?: "UTC");
+  $post->whentime = $this->formatUserTime(intval($post->when), $post->whenzone ?: "UTC");
+  $this->vars['post'] = $post;
+  $this->vars['error'] = $_SESSION['editerror'];
+  unset($_SESSION['editerror']);
+  return;
+}
+
+if ($_SESSION['editerror'] = $this->checkPost($_POST)) {
+  $this->redirect("edit?id={$post->id}");
+}
+
+$post->subreddit = strval($_POST['subreddit']);
+$post->title = strval($_POST['title']);
+$post->body = strval($_POST['body']);
+$post->when = $this->convertTime($_POST['whendate'], $_POST['whentime'], $_POST['whenzone']);
+$post->whenzone = strval($_POST['whenzone']);
+R::store($post);
+$this->redirect("dashboard");
