2709866f3cecf76d206555db951232775938d90d
Kristopher Ives <kristopher.ives@gmail.com>
2018-08-29 13:30:29 +0000

    Added submit.html template. Added submit.php page.

---
+++ pages/submit.php
@@ -0,0 +1,27 @@
+<?php
+
+use \RedBeanPHP\R as R;
+
+$account = $this->getAccount();
+
+if (!isset($_POST['submit'])) {
+  $this->vars['error'] = $_SESSION['submiterror'];
+  unset($_SESSION['submiterror']);
+  return;
+}
+
+if ($_SESSION['submiterror'] = $this->checkPost($_POST)) {
+  $this->redirect("submit");
+}
+
+$post = R::dispense('post');
+$post->account = $account;
+$post->subreddit = strval($_POST['subreddit']);
+$post->title = strval($_POST['title']);
+$post->body = strval($_POST['body']);
+$post->when = $this->convertTime($_POST['whendate'], $_POST['whentime'], $_POST['whenzone']);
+$post->whenzone = strval($_POST['whenzone']);
+$post->url = "";
+R::store($post);
+
+$this->redirect("dashboard");
