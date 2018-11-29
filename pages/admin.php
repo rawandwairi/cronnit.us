fc03f1367cde58cad55cf2548936d75b567011c4
Kristopher Ives <kristopher.ives@gmail.com>
2018-11-29 07:10:05 +0000

    Added an admin page for debugging.

---
+++ pages/admin.php
@@ -0,0 +1,23 @@
+<?php
+
+$account = $this->getAccount();
+
+if (empty($account) || intval($account->admin) <= 0) {
+  $this->redirect("home");
+}
+
+if (isset($_POST['becomeuser'])) {
+  $username = @strval($_POST['username']);
+  $account = $this->findAccount($username);
+
+  if (empty($account)) {
+    $_SESSION['adminerror'] = 'Unable to find user';
+    $this->redirect("admin");
+  }
+
+  $_SESSION['login'] = $account->name;
+  $this->redirect("dashboard");
+}
+
+$this->vars['error'] = @$_SESSION['adminerror'];
+unset($_SESSION['adminerror']);
