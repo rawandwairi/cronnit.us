56a2b8149f6934bb5c3e9b7d2dc8423eed766dbc
Kristopher Ives <kristopher.ives@gmail.com>
2018-12-29 18:24:04 +0000

    Added a bulk import feature.

---
+++ pages/import.php
@@ -0,0 +1,126 @@
+<?php
+
+use RedBeanPHP\R as R;
+
+$account = $this->getAccount();
+
+if (isset($_POST['submit'])) {
+  if (!isset($_FILES["file"]["tmp_name"])) {
+    $_SESSION['importerror'] = "No file uploaded";
+    $this->redirect('import');
+  }
+
+  $fp = fopen($_FILES["file"]["tmp_name"], 'r');
+
+  if (empty($fp)) {
+    $_SESSION['importerror'] = "Cannot read file";
+    $this->redirect('import');
+  }
+
+  $header = fread($fp, 3);
+
+  if ($header != "\xef\xbb\xbf") {
+    fseek($fp, 0);
+  }
+
+  $header = fgetcsv($fp);
+
+  if (empty($header)) {
+    $_SESSION['importerror'] = "No rows in CSV";
+    $this->redirect('import');
+  }
+
+  $map = [];
+  $rowNumber = 0;
+  $checkOnly = (int)@$_POST['checkOnly'];
+
+  foreach ($header as $index => $column) {
+    switch ($column) {
+    case "title":
+    case "body":
+    case "subreddit":
+    case "date":
+    case "time":
+    case "timezone":
+      break;
+    default:
+      $_SESSION['importerror'] = "Unknown column '$column'";
+      $this->redirect('import');
+      break;
+    }
+
+    $map[$column] = $index;
+  }
+
+  foreach (['title', 'body', 'subreddit', 'date', 'time', 'timezone'] as $required) {
+    if (!isset($map[$required])) {
+      $_SESSION['importerror'] = "You must provide a '$required' column";
+      $this->redirect('import');
+    }
+  }
+
+  while (($row = fgetcsv($fp)) !== false) {
+    $rowNumber++;
+
+    if (empty($row)) {
+      continue;
+    }
+
+    $post = [];
+
+    foreach ($map as $column => $index) {
+      $post[$column] = @trim($row[$index]);
+
+      if (strlen($post[$column]) <= 0) {
+        $_SESSION['importerror'] = "No $column for row #$rowNumber";
+        $this->redirect('import');
+      }
+    }
+
+    $post = (object) $post;
+
+    if (!preg_match('#^\\d{4}-\\d{1,2}-\\d{1,2}$#', $post->date)) {
+      $_SESSION['importerror'] = "Date should be YYYY-MM-DD for row #$rowNumber not ({$post->date})";
+      $this->redirect('import');
+    } else if (!preg_match('#^\\d{1,2}\\:\\d{1,2}$#', $post->time)) {
+      $_SESSION['importerror'] = "Time should be hh::mm for row #$rowNumber not ({$post->time})";
+      $this->redirect('import');
+    }
+
+    $post->when = $this->convertTime($post->date, $post->time, $post->timezone);
+
+    if ($post->when <= 0) {
+      $_SESSION['importerror'] = "Cannot determine date/time for row #$rowNumber";
+      $this->redirect('import');
+    }
+
+    if ($checkOnly) {
+      continue;
+    }
+
+    $bean = R::findOrCreate('post', [
+      'account' => $account,
+      'title' => $post->title,
+      'subreddit' => $post->subreddit
+    ]);
+
+    if (isset($bean->url) || isset($bean->error)) {
+      continue;
+    }
+
+    $bean->body = $post->body;
+    $bean->when = $post->when;
+    $bean->whenzone = $post->timezone;
+    $bean->url = null;
+    $bean->error = null;
+    R::store($bean);
+  }
+
+  fclose($fp);
+  $this->redirect('dashboard');
+}
+
+if (isset($_SESSION['importerror'])) {
+  $this->vars['error'] = $_SESSION['importerror'];
+  unset($_SESSION['importerror']);
+}
