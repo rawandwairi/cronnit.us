0515dba037f8033193e6a20ee1b304afb4dbac97
Kristopher Ives <kristopher.ives@gmail.com>
2019-08-07 04:47:29 +0000

    Added multiple views to dashboard. Added calendar view but it needs improvement so it's not enabled yet.

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -3,6 +3,28 @@
 use \RedBeanPHP\R as R;
 
 $account = $this->getAccount();
-
 $this->vars['account'] = $account;
-$this->vars['posts'] = $account->with('order by `when` desc')->ownPostList;
+$posts = $account->with('order by `when` desc')->ownPostList;
+
+switch (@$_GET['view']) {
+case 'calendar':
+  $this->vars['view'] = 'posts-calendar.html';
+  $this->vars['time'] = @$_GET['time'];
+  $indexedPosts = [];
+
+  foreach ($posts as $post) {
+    $year = date('Y', $post->when_original);
+    $month = date('n', $post->when_original);
+    $day = date('j', $post->when_original);
+    $indexedPosts[$year][$month][$day][] = $post;
+  }
+
+  $this->vars['posts'] = $indexedPosts;
+
+  break;
+case 'list':
+default:
+  $this->vars['view'] = 'posts-list.html';
+  $this->vars['posts'] = $posts;
+  break;
+}
